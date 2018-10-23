1dd5aeb20c75329d3f1f0e64f8cf3b7dc89c86d5
NoopurJoshiGA <noopur.joshi13@gmail.com>
2018-10-23 12:19:27 +0100

    transformed middle card

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,84 +7,86 @@ class Carousel extends React.Component {
       className: 'center',
       centerMode: true,
       infinite: true,
-      centerPadding: 0,
-      slidesToShow: 3,
-      // autoplay: true,
+      centerPadding: '30%',
+      slidesToShow: 1,
+      autoplay: true,
       speed: 500,
       responsive: [
         {
           breakpoint: 768,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            centerPadding: '10%',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 615,
+          settings: {
+            centerPadding: '10%',
+            slidesToShow: 1
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            centerPadding: '10px',
+            slidesToShow: 1
           }
         }
       ]
     };
     return (
       <Slider {...settings}>
-          <div className="card">
-            <img src="./assets/turtle.png" />
-            <div className="card-text-top">
-              <h2>Cuba</h2>
-              <h3>Bahamas & caribbean</h3>
-            </div>
-            <div className="card-text-bottom">
-              <h4>View Itinerary</h4>
+        <div className="card">
+          <img src="./assets/turtle.png" />
+          <div className="card-text-top">
+            <h2>Cuba</h2>
+            <h3>Bahamas & caribbean</h3>
+          </div>
+          <div className="card-text-bottom">
+            <h4>View Itinerary</h4>
           </div>
         </div>
-        <div>
-          <div className="card">
-            <img src="./assets/turtle.png" />
-            <div className="card-text-top">
-              <h2>Cuba</h2>
-              <h3>Bahamas & caribbean</h3>
-            </div>
-            <div className="card-text-bottom">
-              <h4>View Itinerary</h4>
-            </div>
+        <div className="card">
+          <img src="./assets/turtle.png" />
+          <div className="card-text-top">
+            <h2>Cuba</h2>
+            <h3>Bahamas & caribbean</h3>
           </div>
-        </div><div>
-          <div className="card">
-            <img src="./assets/turtle.png" />
-            <div className="card-text-top">
-              <h2>Cuba</h2>
-              <h3>Bahamas & caribbean</h3>
-            </div>
-            <div className="card-text-bottom">
-              <h4>View Itinerary</h4>
-            </div>
-          </div>
-        </div><div>
-          <div className="card">
-            <img src="./assets/turtle.png" />
-            <div className="card-text-top">
-              <h2>Cuba</h2>
-              <h3>Bahamas & caribbean</h3>
-            </div>
-            <div className="card-text-bottom">
-              <h4>View Itinerary</h4>
-            </div>
-          </div>
-        </div><div>
-          <div className="card">
-            <img src="./assets/turtle.png" />
-            <div className="card-text-top">
-              <h2>Cuba</h2>
-              <h3>Bahamas & caribbean</h3>
-            </div>
-            <div className="card-text-bottom">
-              <h4>View Itinerary</h4>
-            </div>
+          <div className="card-text-bottom">
+            <h4>View Itinerary</h4>
           </div>
         </div>
+        <div className="card">
+          <img src="./assets/turtle.png" />
+          <div className="card-text-top">
+            <h2>Cuba</h2>
+            <h3>Bahamas & caribbean</h3>
+          </div>
+          <div className="card-text-bottom">
+            <h4>View Itinerary</h4>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./assets/turtle.png" />
+          <div className="card-text-top">
+            <h2>Cuba</h2>
+            <h3>Bahamas & caribbean</h3>
+          </div>
+          <div className="card-text-bottom">
+              <h4>View Itinerary</h4>
+            </div>
+          </div>
+          <div className="card">
+            <img src="./assets/turtle.png" />
+            <div className="card-text-top">
+              <h2>Cuba</h2>
+              <h3>Bahamas & caribbean</h3>
+            </div>
+            <div className="card-text-bottom">
+              <h4>View Itinerary</h4>
+            </div>
+          </div>
       </Slider>
     );
   }
